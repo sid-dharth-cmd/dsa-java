392602d29579ed2ce49cef94ab1997daab468521
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-06 00:12:38 +0530

    editted Q3

--- Assignment-1/Q3.java
+++ Assignment-1/Q3.java
@@ -1,7 +1,7 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 public class Q3 {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+        /*Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
         int temp = n;
@@ -16,5 +16,27 @@ public class Q3 {
         else 
             System.out.println(n+" is not a spy number.");
         sc.close();
+*/
+
+        int n = 1;
+        int temp = n;
+        int sum = 0, product = 1, count = 0;
+        while (true){
+            while(temp != 0){
+                sum += temp % 10;
+                product *= temp % 10;
+                temp /= 10;
+            }
+            if (sum == product){
+                System.out.println(n);
+                count++;
+            }
+            if (count == 50)
+                break;
+            n++;
+            temp = n;
+            sum = 0;
+            product = 1;
+        }
     }
 }
