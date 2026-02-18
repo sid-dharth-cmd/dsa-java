663a0cdd446b917afcc6fad776a7898dcae8e290
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-18 23:30:02 +0530

    OOP

--- Practice/test.java
+++ Practice/test.java
@@ -1,6 +1,17 @@
+package Practice;
+
 class student{
     String name;
     int roll;
     double percentage;
-    
+
+}
+public class test{
+    public static void main(String[] args){
+        student A = new student();
+        A.name = "Ramesh";
+        A.roll = 34;
+        A.percentage = 92.0;
+        
+    }
 }
