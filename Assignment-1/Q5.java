7db047ad145f78ae07e8da8f818c0f5fde3a15de
Siddharth <siddharth.23.2k5@gmail.com>
2026-02-19 22:43:29 +0530

    by using single loop

--- Assignment-1/Q5.java
+++ Assignment-1/Q5.java
@@ -2,17 +2,13 @@ import java.util.Scanner;
 public class Q5 {
     public static int sum_Of_Digits(int n){
         int sum = 0;
-        while (true){
-            while(n != 0){
-                sum += n %10;
-                n /= 10;
-            }
-            if (sum > 9){
+        while (sum > 9 || n > 0){
+            if (n == 0){
                 n = sum;
                 sum = 0;
             }
-            else
-                break;
+            sum += n % 10;
+            n /= 10;
         }
         return sum;
     }
@@ -20,7 +16,7 @@ public class Q5 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n = sc.nextInt();
-        System.out.println("Sum of digits of 9294 until the number is a single digit is "+sum_Of_Digits(n));
+        System.out.println("Sum of digits of "+n+" until the number is a single digit is "+sum_Of_Digits(n));
         sc.close();
     }
 }
