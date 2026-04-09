eb17c66fb3c1343abd68963727a3cabddaf4f2f9
Siddharth <siddharth.23.2k5@gmail.com>
2026-04-09 21:07:00 +0530

    Write a recursive method in Java which, given real  value x and a positive integer n, returns the value of xn.

--- Assignment-3/Q8.java
+++ Assignment-3/Q8.java
@@ -1,5 +1,20 @@
-package Assignment-3;
-
+import java.util.Scanner;
 public class Q8 {
-    
+    public static int pow(int x, int n){
+        if (n == 0)
+            return 1;
+        else
+            return (x * pow(x, n-1));
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter real number(x) and positive integer(n): ");
+        int x = sc.nextInt();
+        int n = sc.nextInt();
+        if (x == 0)
+            System.out.println(x+" to the power "+n+" is 0.");
+        else
+            System.out.println(x+" to the power "+n+" is "+pow(x, n));
+        sc.close();
+    }
 }
